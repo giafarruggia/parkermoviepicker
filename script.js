fb3009045f6d573a1007f69f937bd74a1cef16e0
giafarruggia <giafarruggia@gmail.com>
2026-06-30 11:25:09 -0400

    Update movie display to include length

--- script.js
+++ script.js
@@ -284,7 +284,7 @@ function attachUIEvents() {
         <div>${movie.year ? `${movie.title} (${movie.year})` : movie.title}</div>
 
         <div class="movie-tags">
-            ${movie.year} • ${[...movie.vibes].sort().join(" • ")}
+            ${movie.length} • ${[...movie.vibes].sort().join(" • ")}
         </div>
 
         <button id="watchingBtn">i'm watching this!</button>
